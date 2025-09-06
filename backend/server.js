eb0acfc1f3f44a4363f7c9ecc25ea83a72165184
Akshat774 <akshatsinhal1234@gmail.com>
2025-09-06 22:04:27 +0530

    added new route to send json data

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 
+import etc from './etc.js'
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -7,6 +9,7 @@ const app = express();
 app.use('/', (req, res) => {
 	res.json('This is the backend.');
 });
+app.use(etc)
 
 app.listen(PORT, (req, res) => {
 	console.log(`Server has started on port ${PORT}`);

523f8de8cf6a875224af0dccabcdb4058b812099
SkylerOnAir <blancdiable1@gmail.com>
2025-09-07 14:19:30 +0530

    added otp sender and checker

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -1,4 +1,4 @@
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 
 const HOST = process.env.HOST;
 const USER = process.env.USER;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 
-import { db } from './config/connectDB.js';
+import user from './routes/userRoutes.js';
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.use('/user', user);
 
 app.listen(PORT, () => {
 	console.log(`Server has started on port ${PORT}`);

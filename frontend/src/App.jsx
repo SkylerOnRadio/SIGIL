d5004122d5e5a4bf87631b4d6cfeb3e9778448fc
SkylerOnAir <blancdiable1@gmail.com>
2025-09-07 12:54:52 +0530

    Deleted not working backend

--- frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-	return (
-		<div className="min-h-screen flex items-center justify-center bg-amber-500">
-			<h1 className="text-4xl font-bold text-white">
-				Tailwind Background Test
-			</h1>
-		</div>
-	);
-}

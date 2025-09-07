24e3b8dceaabc49ea7810667f26eaefd2597e7e6
SkylerOnAir <blancdiable1@gmail.com>
2025-09-07 19:25:17 +0530

    idk man

--- backend/etc.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-const router = express.Router()
-
-router.get('/gets', (req, res) => {
-    res.json("This is a route.")
-})
-
-export default router
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import userRoutes from './routes/users.js';
 import productRoutes from './routes/products.js';
@@ -9,6 +10,14 @@ import dashboardRoutes from './routes/dashboard.js';
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(
+	cors({
+		origin: 'http://localhost:5173',
+		credentials: true,
+	})
+);
+app.options('*', cors());
 
 app.use(cookieParser());
 app.use(express.json());

9521e3dba9039b62f57537609508f212af448940
SkylerOnAir <blancdiable1@gmail.com>
2025-09-07 18:39:12 +0530

    removed postcss

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwind from '@tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tailwind()],
 });

4ff67b335f7ac4ffcbbb057add6ad7fe11757ebb
SkylerOnAir <blancdiable1@gmail.com>
2025-09-07 14:23:57 +0530

    removed redundant routes

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,13 @@
 import express from 'express';
 
-import userRoutes from "./routes/users.js";
+import userRoutes from './routes/users.js';
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-
 // Mount routes
-app.use("/users", userRoutes);
-
-app.use('/', (req, res) => {
-	res.json('This is the backend.');
-});
-app.use(etc)
-
+app.use('/users', userRoutes);
 
 app.use(express.json());
 app.use(express.urlencoded());
@@ -22,6 +15,5 @@ app.use(express.urlencoded());
 app.use('/user', user);
 
 app.listen(PORT, () => {
-
 	console.log(`Server has started on port ${PORT}`);
 });

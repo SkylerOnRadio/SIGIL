1bd193dffc1bf2392c4fd5e9020b435037910cb4
mai-VAIBHAV <vaibhavtiwari10505@gmail.com>
2025-09-07 17:40:05 +0530

    all done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <>
+    <StrictMode>
+      <App />
+    </StrictMode>
+    ,
+  </>
+);

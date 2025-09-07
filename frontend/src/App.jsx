79a743be86791fbf1ad4161a66d4e33733ae161c
mai-VAIBHAV <vaibhavtiwari10505@gmail.com>
2025-09-07 19:05:05 +0530

    added a Product creation page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,26 @@
 import {
-	createBrowserRouter,
-	createRoutesFromElements,
-	Route,
-	RouterProvider,
-} from 'react-router-dom';
-import MainLayout from './Layouts/MainLayout';
-import ProductPage from './Pages/ProductPage';
-import Register from './Pages/RegisterPage';
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from "./Layouts/MainLayout";
+import ProductPage from "./Pages/ProductPage";
+import Register from "./Pages/RegisterPage";
+import ProductCreation from "./Pages/ProductCreation";
 
 const App = () => {
-	const router = createBrowserRouter(
-		createRoutesFromElements(
-			<Route path="/" element={<MainLayout />}>
-				<Route path="/" element={<ProductPage />} />
-				<Route path="register" element={<Register />} />
-			</Route>
-		)
-	);
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route path="/" element={<ProductPage />} />
+        <Route path="register" element={<Register />} />
+        <Route path="/ProductCreation" element={<ProductCreation />} />
+      </Route>
+    )
+  );
 
-	return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 };
 
 export default App;

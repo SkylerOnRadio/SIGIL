452ea51ce284d3ebd88fa181ab64846cf62105a8
SkylerOnAir <blancdiable1@gmail.com>
2025-09-07 19:34:29 +0530

    g

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import path from 'path';
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import cors from 'cors';
 
 import userRoutes from './routes/users.js';
 import productRoutes from './routes/products.js';
@@ -10,14 +9,6 @@ import dashboardRoutes from './routes/dashboard.js';
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-
-app.use(
-	cors({
-		origin: 'http://localhost:5173',
-		credentials: true,
-	})
-);
-app.options('*', cors());
 
 app.use(cookieParser());
 app.use(express.json());
